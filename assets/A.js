e3f6b421de46c32a948d751a7e048873311b6455
FatOldEight <tzq80820@163.com>
2023-05-16 16:23:10 +0800

    update

--- assets/A.js
+++ assets/A.js
@@ -4,7 +4,16 @@ const CryptoJS = require("crypto-js");
 var data ="<p>这里没有东西</p>"
 
 //秘钥
-var aesKey = "c4d038b4bed09fdb1471ef51ec3a32cd";
+var Key = "114514";
+
+//验证码
+
+var check = CryptoJS.SHA1(Key);
+console.log(check.toString());
+
+//二次密钥
+
+var aesKey = CryptoJS.MD5(Key).toString();
 
 //将秘钥转换成Utf8字节数组
 var key = CryptoJS.enc.Utf8.parse(aesKey);
